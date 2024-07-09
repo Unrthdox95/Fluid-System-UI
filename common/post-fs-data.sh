f8ed1ecba4db7c1eec1e713c061bf3f7b34b44df
Unrthdox95 <bhogsrider@gmail.com>
2024-07-09 20:51:20 +0800

    Update post-fs-data.sh

--- common/post-fs-data.sh
+++ common/post-fs-data.sh
@@ -4,6 +4,13 @@
 # and module is placed.
 # This will make sure your module will still work
 # if Magisk change its mount point in the future
-MODDIR=${0%/*}
 
+
+function setx() {
+  [[ ! -x "${0%/*}/$1" ]] && chmod +x "${0%/*}/$1"
+}
+  setx "system/bin/gm1"
+  setx "system/bin/JIT compiler"
+  setx "system/bin/Pixelr"
+  setx "/bin/shexample"
 # This script will be executed in post-fs-data mode
